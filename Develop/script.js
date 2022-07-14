edcc6d9255d60c810e1618c849c4cee902c39d79
rjschroeder <54991779+rjschroeder@users.noreply.github.com>
2022-07-14 14:44:30 -0500

    finished generatePassword function
    Works for almost all extra user input handling, check for NaN, independently searches input string for character types for password criteria, and generates password using Math.random. Also made sure validateCharTypeInput worked properly.

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,7 @@ var CHARS = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "012345
 function generatePassword() {
   var lengthInput = 0;
   var charTypeInput = " ";
+  var pass = "";
   while(lengthInput < 8 || lengthInput > 128 || isNaN(lengthInput)) {
     lengthInput = parseInt(prompt("Please type in a length for your password. Enter a number of at least 8, but no more than 128. This prompt will repeat until a number is entered within this range."), 10);
   }
@@ -14,11 +15,14 @@ function generatePassword() {
     charTypeInput = prompt("Please type in which character types to include. Input is NOT case-sensitive, and character types can be ordered however you please. The options are: Lowercase, Uppercase, Numeric, and Special for special characters.").toLowerCase();
     //The parameters for the replace function were not working as I wanted them to so I found this solution on the internet where
     //the \s represents whitespace and \g implies global, ensuring all of the white space in the string is removed and everything is joined.
-    //This helps with different user input, so "lowercase" and "lower    case" would both be valid inputs.
+    //This helps with different user input, so "lowercase", "lower case", and "lower    case" would all be valid inputs.
     charTypeInput = charTypeInput.replace(/\s/g, '');
   }
-  console.log(charTypeInput);
-  console.log(validateCharTypeInput(charTypeInput));
+  var charsToUse = validateCharTypeInput(charTypeInput);
+  for (var i = 0; i < lengthInput; i++){
+    pass += charsToUse[Math.floor(Math.random() * charsToUse.length)];
+  }
+  return pass;
 }
 
 function validateCharTypeInput(Input){
